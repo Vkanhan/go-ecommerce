1bb8d30927b60591f6d8130ec13279fbc8bebc05
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-09-26 07:29:54 +0530

    feat: Add handler to retrieve user by API key and integrate with routes

--- handler_user.go
+++ handler_user.go
@@ -6,10 +6,12 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/Vkanhan/go-aggregator/internal/auth"
 	"github.com/Vkanhan/go-aggregator/internal/database"
 	"github.com/google/uuid"
 )
 
+// handlerCreateUserreads the user's name from the request body, generates a new user ID (UUID) and inserts the user into the database.
 func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Name string `json:"name"`
@@ -19,7 +21,7 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	params := parameters{}
 	err := decoder.Decode(&params)
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+		respondWithError(w, 400, fmt.Sprintf("Invalid JSON: %v", err))
 		return
 	}
 	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
@@ -30,6 +32,22 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	})
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
+		return
+	}
+	respondWithJSON(w, 200, databaseUserToUse(user))
+}
+
+// handlerGetUser retrieves a user based on the API key provided in the request header.
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Counldn't get the user: %v", err))
 		return
 	}
 	respondWithJSON(w, 200, databaseUserToUse(user))
